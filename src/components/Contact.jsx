fd15e659707d5634c19823e86c5b9bbd82f7882b
Nixyz11 <nikola.bogdanovic.ssfon@gmail.com>
2023-03-14 21:20:19 +0100

    commit 2 - no change

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -55,7 +55,7 @@ const Contact = () => {
         () => {
           setLoading(false);
           alert("Thank you. I will get back to you as soon as possible.");
-
+ 
           setForm({
             name: "",
             email: "",
